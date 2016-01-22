83f4b95e6013634a8b6818e58de4a20d999faf58
tuwanli <909111806@qq.com>
2016-01-22 16:54:14 +0800

    增加搜索
    增加搜索

--- ChineseToChar/ViewController.m
+++ ChineseToChar/ViewController.m
@@ -9,8 +9,13 @@
 #import "ViewController.h"
 #define KCNSSTRING_ISEMPTY(str) (str == nil || [str isEqual:[NSNull null]] || str.length <= 0)
 
-@interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
+@interface ViewController ()<UITableViewDataSource,UITableViewDelegate,UISearchResultsUpdating>
+{
+
+    UISearchController *seachVC;
+}
 @property (nonatomic,retain)NSArray *dataArray;
+@property (nonatomic,retain)NSMutableArray *littleArray;
 @property (nonatomic,retain)NSDictionary *allKeysDict;
 @property (nonatomic,retain)UITableView *tableView;
 @end
@@ -37,14 +42,21 @@
         return NSOrderedDescending;
     }];
     
+    
 }
 
 - (void)CreateUI
 {
+    seachVC = [[UISearchController alloc]initWithSearchResultsController:nil];
+    seachVC.searchResultsUpdater = self;
+    seachVC.dimsBackgroundDuringPresentation = false;
+    [seachVC.searchBar sizeToFit];
+
     _tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
     _tableView.delegate = self;
     _tableView.dataSource = self;
     _tableView.sectionIndexColor = [UIColor redColor];
+    _tableView.tableHeaderView = seachVC.searchBar;
     [self.view addSubview:_tableView];
 }
 - (NSDictionary *)createCharacter:(NSMutableArray *)strArr
@@ -75,24 +87,50 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-
-    return self.dataArray.count;
+//
+    if (!seachVC.active) {
+        return self.dataArray.count;
+    }else{
+        return 1;
+        
+    }
 }
 
-//创建右侧索引表，返回需要显示的索引表数组
 - (NSArray *) sectionIndexTitlesForTableView:(UITableView *)tableView {
-    return self.dataArray;
+    if (!seachVC.active) {
+        return self.dataArray;
+    }else{
+        return _littleArray;
+    }
+    
 }
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-//    NSLog(@"%ld",[(NSArray*)self.allKeysDict[self.dataArray[section]] count]);
+    if (!seachVC.active) {
+        return [(NSArray*)self.allKeysDict[self.dataArray[section]] count];
+    }else{
+    
+        if (_littleArray.count!=0) {
+             return [(NSArray*)self.allKeysDict[self.littleArray[section]] count];
+        }else{
+        
+            return 0;
+        }
+       
+    }
 
-    return [(NSArray*)self.allKeysDict[self.dataArray[section]] count];
+    
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    return self.dataArray[section];
+    if (!seachVC.active) {
+        return self.dataArray[section];
+    }else{
+    
+        return nil;
+    }
+    
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -101,10 +139,26 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
     }
-    cell.textLabel.text = [self.allKeysDict[self.dataArray[indexPath.section]] objectAtIndex:indexPath.row];
+    if (!seachVC.active) {
+        cell.textLabel.text = [self.allKeysDict[self.dataArray[indexPath.section]] objectAtIndex:indexPath.row];
+    }else{
+    
+        cell.textLabel.text = [self.allKeysDict[self.littleArray[indexPath.section]] objectAtIndex:indexPath.row];
+    }
+    
     return cell;
 }
+#pragma mark - searchController delegate
 
+- (void)updateSearchResultsForSearchController:(UISearchController *)searchController {
+//    [self.littleArray removeAllObjects];
+    NSPredicate *searchPredicate = [NSPredicate predicateWithFormat:@"SELF CONTAINS[c] %@",searchController.searchBar.text];
+    self.littleArray = [[self.dataArray filteredArrayUsingPredicate:searchPredicate] mutableCopy];
+    NSLog(@"--------------------%@",_littleArray);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.tableView reloadData];
+    });
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
